c63404644e12639bf779e97acd33a16c2b97e192
lyung96 <70207526+lyung96@users.noreply.github.com>
2021-06-15 21:18:08 +0800

    testing delete

--- Assets/Scripts/Testing1.cs
+++ Assets/Scripts/Testing1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class mygamemovement : MonoBehaviour
+public class Testing1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
