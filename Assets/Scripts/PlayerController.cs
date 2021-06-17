454bd24cff2efe5d469f0f284f577b26e4a19795
Jeffcatdragorn <cat.dragron@gmail.com>
2021-06-18 04:41:18 +0800

    scoring system when collide/triggered with items
    everything seems fine, but there is a bug for the scoring. there is 5 items, score should be 5 if each item is assign with 1 score.  but somehow sometimes it detects it twice, so you sometimes will end up having 6 to 7 scores instead of 5.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class PlayerController : MonoBehaviour
     public float Speed = 3.0f;
     public float JumpForce = 10f;
     public bool isGrounded = true;
+    public Text scoring;
+    public static int scoreNum;
     //public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -31,14 +34,14 @@ public class PlayerController : MonoBehaviour
             gameObject.GetComponent<SpriteRenderer>().flipX = true;
             //anim.SetBool("running", true);
             //gameObject.transform.localScale =new Vector2(-1, 1);
-        }  else if (Input.GetKey(KeyCode.D))
+        } else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(Speed, rb.velocity.y);
             //gameObject.transform.Translate(new Vector2(1.0f, 0.0f) * Time.deltaTime * Speed);
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
             //anim.SetBool("running", true);
             //gameObject.transform.localScale = new Vector2(1, 1);
-        }  else
+        } else
         {
             //anim.SetBool("running", false);
         }
@@ -56,11 +59,27 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground")
         {
-            Debug.Log("Touched ");
+            Debug.Log("Touch Ground ");
             isGrounded = true;
-        }     
+        }
+
     }
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        //there is a BUG!!! in here, cant find why is it detecting twice when it should detect only once. 
+        //player touch, items dies = 1 touch 
+        //system detects as - player touch, item still alive, player touch, item dies = 2 touch
+        if (trigger.gameObject.tag == "Items")
+        {
+            Debug.Log("Triggered White Mask");
+            scoreNum++;
+            scoring.text = "Score: " + scoreNum;
+            Destroy(trigger.gameObject);
+        }
+    }
+        
+
 
 }
