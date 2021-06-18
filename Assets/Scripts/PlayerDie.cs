4cfdeb08cc3fb53e9ccbf37037eb33349c73196c
Jeffcatdragorn <cat.dragron@gmail.com>
2021-06-18 22:15:29 +0800

    player die
    if player falls, they die. game will be restarted

---
+++ Assets/Scripts/PlayerDie.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDie : MonoBehaviour
+{
+    public GameObject player;
+    public bool playerDied = false;
+    public static int scoreNum;
+
+    void Update()
+    {
+
+
+        if (player.transform.position.y < -7)
+        {
+            playerDied = true;
+        }
+        if (playerDied == true)
+        {
+            StartCoroutine("Die");
+        }
+    }
+    IEnumerator Die()
+    {
+
+        SceneManager.LoadScene("GameLevel1");
+        yield return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
