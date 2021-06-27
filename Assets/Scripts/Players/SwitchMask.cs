f2785370a33b63fd8b5de2fefaf49581ba985574
loozhengjie <85921588+loozhengjie@users.noreply.github.com>
2021-06-27 22:54:54 +0800

    Update SwitchMask.cs

--- Assets/Scripts/Players/SwitchMask.cs
+++ Assets/Scripts/Players/SwitchMask.cs
@@ -24,27 +24,20 @@ public class SwitchMask : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if 1 is pressed
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            activatemask1();
-            //deaactivatemask2();
-            //deaactivatemask3();
+            activatemask1();//set mask1 active, mask2 and mask 3 inactive
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            //activatemask2();
-            deaactivatemask1();
-            //deaactivatemask3();
+            activatemask2();//set mask2 active, mask1 and mask 3 inactive
         }
-        //    else if (Input.GetKeyDown(KeyCode.Alpha3))
-        //    {
-        //        activatemask3();
-        //        deaactivatemask1();
-        //        deaactivatemask2();
-        //    }
-        //}
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            activatemask3();//set mask3 active, mask1 and mask 2 inactive
+        }
     }
-
     public void activatemask1()
     {
         mask1.SetActive(true);
@@ -55,65 +48,26 @@ public class SwitchMask : MonoBehaviour
         mask2active = false;
         mask3active = false;
     }
-    public void deaactivatemask1()
+
+    public void activatemask2()
     {
+        mask2.SetActive(true);
+        Debug.Log("Mask 2 Activated");
+        mask2active = true;
         mask1.SetActive(false);
-        Debug.Log("Mask 1 deActivated");
+        mask3.SetActive(false);
         mask1active = false;
-        if (mask2active == true)
-        {
-            mask3active = false;
-        }
-        else if (mask3active == true)
-        {
-            mask2active = false;
-        }
+        mask3active = false;
     }
-
-    //    public void activatemask2()
-    //    {
-    //        mask1.SetActive(true);
-    //        Debug.Log("Mask 2 Activated");
-    //        mask2active = true;
-    //        mask1active = false;
-    //        mask3active = false;
-    //    }
-    //    public void deaactivatemask2()
-    //    {
-    //        mask1.SetActive(false);
-    //        Debug.Log("Mask 2 deActivated");
-    //        mask2active = false;
-    //        if (mask1active == true)
-    //        {
-    //            mask3active = false;
-    //        }
-    //        else if (mask3active == true)
-    //        {
-    //            mask1active = false;
-    //        }
-    //    }
-
-    //    public void activatemask3()
-    //    {
-    //        mask1.SetActive(true);
-    //        Debug.Log("Mask 3 Activated");
-    //        mask3active = true;
-    //        mask1active = false;
-    //        mask2active = false;
-    //    }
-    //    public void deaactivatemask3()
-    //    {
-    //        mask1.SetActive(false);
-    //        Debug.Log("Mask 3 deActivated");
-    //        mask3active = false;
-    //        if (mask2active == true)
-    //        {
-    //            mask1active = false;
-    //        }
-    //        else if (mask1active == true)
-    //        {
-    //            mask2active = false;
-    //        }
-    //    }
-    //}
+    
+    public void activatemask3()
+    {
+        mask3.SetActive(true);
+        Debug.Log("Mask 3 Activated");
+        mask3active = true;
+        mask2.SetActive(false);
+        mask1.SetActive(false);
+        mask1active = false;
+        mask2active = false;
+    }
 }
