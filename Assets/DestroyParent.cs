7bf4b20fd9e45c59691c966e2df9082e272c89f9
lyung96 <70207526+lyung96@users.noreply.github.com>
2021-08-14 16:21:50 +0800

    Week 11 EnemyPlacement and exp

--- Assets/DestroyParent.cs
+++ Assets/DestroyParent.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class DestroyParent : MonoBehaviour
 {
+
+    public int exp = 10;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Destroy(transform.parent.gameObject);
-            PlayerController.exp += 1;
+            PlayerController.exp += exp;
         }
     }
 }
